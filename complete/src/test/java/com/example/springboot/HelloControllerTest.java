60ac3df3caf2630163a1a2db4467f33110b785b5
crawfordsolutions <59573004+crawfordsolutions@users.noreply.github.com>
2020-08-30 21:28:21 -0600

    Fixing Jenkinsfile.

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.Assert.fail;
+import org.junit.Assert.fail;
 
 
 @SpringBootTest
