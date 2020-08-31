d86e595a888aeb0618b64ca94e7ecbb9c9fcfcef
crawfordsolutions <59573004+crawfordsolutions@users.noreply.github.com>
2020-08-30 21:24:17 -0600

    Fixing Jenkinsfile.

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -13,6 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.Assert.fail;
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
@@ -25,6 +28,11 @@ public class HelloControllerTest {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testNullInput() {
+		fail(nullString.indexOf(3));
 	}
 }
 
