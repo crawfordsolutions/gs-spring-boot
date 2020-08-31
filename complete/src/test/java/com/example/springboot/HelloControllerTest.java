bcf6b307706a57542e224f7c6565e8d26b641d31
crawfordsolutions <59573004+crawfordsolutions@users.noreply.github.com>
2020-08-31 12:44:29 -0600

    Fixing Jenkinsfile.

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -24,6 +24,12 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("reetings from Spring Boot!")));
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
+
+	@Test(expected = NullPointerException.class)
+public void test () throws NullPointerException {
+    Foo f = new Foo();
+    f.methodThatThrowsException();
+}
 }
